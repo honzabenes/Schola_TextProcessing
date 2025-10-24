d7a74a84ae34a8c6a9036b8b9186991fd07e8f77
Jan <jan.b3nes@gmail.com>
2025-10-24 17:29:34 +0200

    Add TokenReader abstract class

---
+++ TokenReader.cs
@@ -0,0 +1,14 @@
+﻿namespace TextProcessing
+{
+    public abstract class TokenReader
+    {
+        private TextReader _reader;
+
+        public TokenReader(TextReader reader)
+        {
+            _reader = reader;
+        }
+
+        public abstract Token ReadToken();
+    }
+}
