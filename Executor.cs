ebaea78b14fa44eb1492b209f7e2106cd863de46
Jan <jan.b3nes@gmail.com>
2025-10-25 12:22:58 +0200

    Add Executor static class

---
+++ Executor.cs
@@ -0,0 +1,18 @@
+﻿namespace TextProcessing
+{
+    public static class Executor
+    {
+        public static void ProcessAllWords(TokenReader reader, ITokenProcessor processor)
+        {
+            Token token = reader.ReadToken();
+
+            while (token.Type != TypeToken.EoI)
+            {
+                processor.ProcessToken(token);
+                token = reader.ReadToken();
+            }
+
+            processor.WriteOut();
+        }
+    }
+}
