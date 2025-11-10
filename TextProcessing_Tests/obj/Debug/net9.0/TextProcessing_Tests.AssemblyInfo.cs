e3046068659d6be7f3148e3672dd62bb91299c87
Jan Beneš <jan.benes@mailportal.cz>
2025-11-10 13:20:59 +0100

    commit

--- TextProcessing_Tests/obj/Debug/net9.0/TextProcessing_Tests.AssemblyInfo.cs
+++ TextProcessing_Tests/obj/Debug/net9.0/TextProcessing_Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextProcessing_Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1841a517f0162e48299f45b955f76ea2adbe7151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021058f6dffe33edb0a0f92ee4a2d5caff8ee359")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextProcessing_Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextProcessing_Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
