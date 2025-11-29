28723ec44774c39f8150bbe6295b90dd01e7d092
Jan <jan.b3nes@gmail.com>
2025-11-29 16:38:02 +0100

    Add MultifileTokenReader

---
+++ src/Frameworks/TokenProcessingFramework/MultifileTokenReader.cs
@@ -0,0 +1,46 @@
+﻿namespace TokenProcessingFramework
+{
+    public class MultifileTokenReader : ITokenReader, IDisposable
+    {
+        private ITokenReader? _currentReader;
+        private ITokenReaderFactory _factory;
+
+        public MultifileTokenReader(ITokenReaderFactory factory)
+        {
+            _factory = factory;
+        }
+
+
+        public Token ReadToken()
+        {
+            if (_currentReader is null)
+            {
+                _currentReader = _factory.GetNextReader();
+                if ( _currentReader is null )
+                {
+                    return new Token(TokenType.EoI);
+                }
+            }
+
+            Token token = _currentReader.ReadToken();
+
+            if (token.Type == TokenType.EoI)
+            {
+                Dispose();
+                ReadToken();
+            }
+
+            return token;
+        }
+
+
+        public void Dispose()
+        {
+            if ( _currentReader is IDisposable disposable )
+            {
+                disposable.Dispose();
+            }
+            _currentReader = null;
+        }
+    }
+}
