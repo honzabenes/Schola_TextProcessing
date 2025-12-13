1201f8ac75834ea9533afa47ce0cab7529f3479f
Jan <jan.b3nes@gmail.com>
2025-12-13 18:39:17 +0100

    Add Huffman I solution

---
+++ src/Tests/FileByteReaderToTokenReaderAdapter_UnitTests/FileByteReaderToTokenReaderAdapter_UnitTests.cs
@@ -0,0 +1,117 @@
+﻿using ByteProcessingFramework;
+using TokenProcessingFramework;
+
+namespace HuffmanTree_UnitTests
+{
+    public class FileByteReaderToTokenReaderAdapter_UnitTests
+    {
+        private class FakeByteReader : IByteReader
+        {
+            private byte[] _data;
+            private int _position;
+
+            public FakeByteReader(int dataSize)
+            {
+                _data = new byte[dataSize];
+                
+                var random = new Random();
+                random.NextBytes(_data);
+
+                _position = 0;
+            }
+
+            public FakeByteReader(byte[] data)
+            {
+                _data = data;
+                _position = 0;
+            }
+
+
+            public byte? ReadByte()
+            {
+                if (_position >= _data.Length)
+                {
+                    return null;
+                }
+
+                return _data[_position++];
+            }
+        }
+
+        public List<Token> ReadAllTokens(ITokenReader reader)
+        {
+            var readTokens = new List<Token>();
+
+            Token token;
+            while ((token = reader.ReadToken()) is not { Type: TokenType.EoI })
+            {
+                readTokens.Add(token);
+            }
+            readTokens.Add(token);
+
+            return readTokens;
+        }
+
+
+        [Fact]
+        public void EmptyInput()
+        {
+            // Arrange
+            IByteReader fakeByteReader = new FakeByteReader(0);
+            var adapter = new FileByteReaderToTokenReaderAdapter(fakeByteReader);
+
+            // Act
+            var readTokens = ReadAllTokens(adapter);
+
+
+            // Assert
+            List<Token> expectedTokens = new List<Token>{
+                new Token(TokenType.EoI)
+            };
+
+            Assert.Equal(expectedTokens, readTokens);
+        }
+
+
+        [Fact]
+        public void OneByte()
+        {
+            // Arrange
+            IByteReader fakeByteReader = new FakeByteReader([101]);
+            var adapter = new FileByteReaderToTokenReaderAdapter(fakeByteReader);
+
+            // Act
+            var readTokens = ReadAllTokens(adapter);
+            
+            // Assert
+            List<Token> expectedTokens = new List<Token>{ 
+                new Token("101"), 
+                new Token(TokenType.EoI) 
+            };
+
+            Assert.Equal(expectedTokens, readTokens);
+        }
+
+
+        [Fact]
+        public void MultipleBytes()
+        {
+            // Arrange
+            IByteReader fakeByteReader = new FakeByteReader([101, 26, 51]);
+            var adapter = new FileByteReaderToTokenReaderAdapter(fakeByteReader);
+
+            // Act
+            var readTokens = ReadAllTokens(adapter);
+
+            // Assert
+            List<Token> expectedTokens = new List<Token>{
+                new Token("101"),
+                new Token("26"),
+                new Token("51"),
+                new Token(TokenType.EoI)
+            };
+
+            Assert.Equal(expectedTokens, readTokens);
+        }
+    }
+}
---
+++ src/Tests/FileByteReaderToTokenReaderAdapter_UnitTests/NodeComparing_UnitTests.cs
@@ -0,0 +1,111 @@
+﻿using HuffmanTreeFramework;
+
+namespace HuffmanTree_UnitTests
+{
+    public class NodeComparing_UnitTests
+    {
+        [Fact]
+        public void Nodes_DifferentWeights_SmallerWeightComesFirst()
+        {
+            // Arrange
+            Node lighterNode = new Node(5, 10, null, null);
+            Node heavierNode = new Node(1, 15, null, null);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void Nodes_EqualWeights_OlderComesFirst()
+        {
+            // Arrange
+            Node lighterNode = new Node(1, 15, null, null);
+            Node heavierNode = new Node(5, 15, null, null);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void Leafs_DifferentWeights_SmallerWeightComesFirst()
+        {
+            // Arrange
+            Leaf lighterNode = new Leaf(97, 10);
+            Leaf heavierNode = new Leaf(97, 15);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void Leafs_EqualWeights_SmallerValueComesFirst()
+        {
+            // Arrange
+            Leaf lighterNode = new Leaf(97, 10);
+            Leaf heavierNode = new Leaf(101, 10);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void LeafVSNode_LeafHeavier_NodeComesFirst()
+        {
+            // Arrange
+            Node lighterNode = new Node(5, 10, null, null);
+            Leaf heavierNode = new Leaf(97, 15);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void LeafVSNode_NodeHeavier_LeafComesFirst()
+        {
+            // Arrange
+            Leaf lighterNode = new Leaf(97, 10);
+            Node heavierNode = new Node(5, 15, null, null);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+
+
+        [Fact]
+        public void LeafVSNode_EqualWeights_LeafComesFirst()
+        {
+            // Arrange
+            Leaf lighterNode = new Leaf(0, 10);
+            Node heavierNode = new Node(5, 10, null, null);
+
+            // Act
+            int result = lighterNode.CompareTo(heavierNode);
+
+            // Assert
+            Assert.True(result < 0);
+        }
+    }
+}
