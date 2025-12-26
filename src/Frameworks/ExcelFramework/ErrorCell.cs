3f9ee544537757c738dd1f7cf38bf9f7616d5ca6
Jan <jan.b3nes@gmail.com>
2025-12-26 14:47:39 +0100

    Add ErrorCell

---
+++ src/Frameworks/ExcelFramework/ErrorCell.cs
@@ -0,0 +1,24 @@
+﻿namespace ExcelFramework
+{
+    public record ErrorCell : Cell
+    {
+        public string ErrorMessage;
+
+        public ErrorCell(string errorMessage) : base(0, CellState.Error)
+        {
+            ErrorMessage = errorMessage;
+        }
+
+
+        public override string GetOutputString()
+        {
+            return ErrorMessage;
+        }
+
+
+        public override int GetValue(Sheet sheet)
+        {
+            return 0;
+        }
+    }
+}
